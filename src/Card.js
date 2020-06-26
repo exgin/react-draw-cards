aa0de8cf1596b3bca95ce21f374ad5b838a12ec4
unknown <exgin3538@yahoo.com>
2020-06-26 14:41:04 -0400

    add timer

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
+import './Card.css';
 
 const Card = ({ image }) => {
-  return <img src={image} alt='card' />;
+  // the card's angle is changing, so we need to use state
+  const [{ ang }] = useState({ ang: Math.random() * 90 - 45 });
+
+  const rotateCard = `transfom: rotate(${ang}deg)`;
+
+  return <img className='Card' src={image} alt='card' stlye={{ rotateCard }} />;
 };
 
 export default Card;
--- src/Cards.js
+++ src/Cards.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { v4 as uuid } from 'uuid';
 import Card from './Card';
@@ -20,6 +20,8 @@ const Cards = () => {
   // changing cards
   const [cards, setCards] = useState(null);
   const [cardDrawn, setCardDrawn] = useState([]);
+  const [autoDraw, setAutoDraw] = useState(false);
+  const timerId = useRef();
 
   const BASE_URL = `https://deckofcardsapi.com/api/deck`;
   const NEW_DECK = `${BASE_URL}/new/shuffle/`;
@@ -33,27 +35,57 @@ const Cards = () => {
     newData();
   }, [setCards]); // when our res.data changes, we get a new deck shuffled
 
-  const drawCard = async () => {
-    const { deck_id } = cards; // destrcut from our 'global' state in useEffect
-    try {
-      const singleCard = await axios.get(`${BASE_URL}/${deck_id}/draw/?count=1`);
+  useEffect(() => {
+    const drawCard = async () => {
+      const { deck_id } = cards; // destrcut from our 'global' state in useEffect
+      try {
+        const singleCard = await axios.get(`${BASE_URL}/${deck_id}/draw/?count=1`);
 
-      if (singleCard.data.remaining === 0) {
-        alert('No Cards Remaining!');
-        throw new Error('No Cards Remaining!');
+        if (singleCard.data.remaining === 0) {
+          alert('No Cards Remaining!');
+          throw new Error('No Cards Remaining!');
+        }
+        // adding to our cardsDrawn array data structure
+        setCardDrawn((cardDrawn) => [...cardDrawn, { ...singleCard.data, id: uuid() }]);
+      } catch (error) {
+        console.error(error);
       }
+    };
 
-      // adding to our cardsDrawn array data structure
-      setCardDrawn((cardDrawn) => [...cardDrawn, { ...singleCard.data, id: uuid() }]);
-    } catch (error) {
-      console.error(error);
+    // if autoDraw is fasle & timer isn't made, make one | => draw a card every second
+    if (autoDraw && !timerId.current) {
+      timerId.current = setInterval(async () => {
+        await drawCard();
+      }, 1000);
     }
+
+    timerId.current = setInterval(() => {
+      setAutoDraw((seconds) => seconds + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId.current); // stop timer
+      timerId.current = null; // set obj to null
+    };
+  }, [autoDraw, setAutoDraw, cards]);
+
+  const stopTimer = () => {
+    clearInterval(timerId.current);
+  };
+
+  const toggleDrawing = () => {
+    setAutoDraw((drawing) => !drawing);
   };
 
   return (
     <div className='Cards'>
       <h1>Deck of Cards Picker</h1>
-      <button onClick={drawCard}>Give Me a Card!</button>
+      <button className='Cards-btn' onClick={toggleDrawing}>
+        Give Me a Cards!
+      </button>
+      <button className='Cards-stop' onClick={stopTimer}>
+        Stop drawing
+      </button>
       <div className='Cards-spot'>
         {cardDrawn.map((card) => (
           <Card key={card.id} image={card.cards[0].image} />
